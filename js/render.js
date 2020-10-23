6f3c54fae72de310fa29768542168d3072cb78da
Mike-Cursor <63852043+Mike-Cursor@users.noreply.github.com>
2020-10-23 17:31:54 +0300

    чиним структуру данных

--- js/render.js
+++ js/render.js
@@ -1,35 +1,29 @@
 (function () {
-    const BUTTON_BRIGHTNESS = {
-        normal: 1,
-        mouseOver: 0.5,
-        mouseDown: 0.2,
-    };
-    window.renderGame = function(levelNumber, data) {
+    window.renderGame = function(levelNumber, pictureData, viewData) {
         //igra
         const gameElement = document.querySelector('body');
         //karta
         const mapElement = window.makeMap();
         gameElement.appendChild(mapElement);
         //knopki
-        const buttonElements = window.makeButtons(levelNumber, mapElement);
+        const buttonElements = window.makeButtons(levelNumber, viewData.button);
         buttonElements.forEach(button => {
             button.addEventListener('mouseover', function() {
-                button.style.filter = `brightness(${BUTTON_BRIGHTNESS.mouseOver})`;
+                button.style.filter = `brightness(${viewData.button.brightness.mouseOver})`;
             });
             button.addEventListener('mouseout', function() {
-                button.style.filter = `brightness(${BUTTON_BRIGHTNESS.normal})`;
+                button.style.filter = `brightness(${viewData.button.brightness.normal})`;
             });
             button.addEventListener('mousedown', function() {
-                button.style.filter = `brightness(${BUTTON_BRIGHTNESS.mouseDown})`;
+                button.style.filter = `brightness(${viewData.button.brightness.mouseDown})`;
             });
             button.addEventListener('mouseup', function() {
-                button.style.filter = `brightness(${BUTTON_BRIGHTNESS.mouseOver})`;
-                
+                button.style.filter = `brightness(${viewData.button.brightness.mouseOver})`;
             });
             gameElement.appendChild(button);
         });
         //kartinki
-        const pictureElements = window.makePictures(data);
+        const pictureElements = window.makePictures(pictureData);
         pictureElements.forEach(picture => {
             gameElement.appendChild(picture.item);
             gameElement.appendChild(picture.information);
